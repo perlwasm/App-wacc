13cbc3f76c0f87e3b578ab521411dda81a4435ac
Graham Ollis <plicease@cpan.org>
2020-04-13 13:29:20 -0600

    export and import

--- examples/mainless-wasm/foo.c
+++ examples/mainless-wasm/foo.c
@@ -1,6 +1,21 @@
+/*
+ * This was helpful:
+ * https://lld.llvm.org/WebAssembly.html
+ * https://clang.llvm.org/docs/AttributeReference.html#import-module
+ */
+
+#define IMPORT_FOO __attribute__((import_module("foo")))
 #define EXPORT __attribute__ ((visibility ("default")))
+
+IMPORT_FOO extern void bar();
 
 EXPORT int add(int a, int b)
 {
+  bar();
   return a+b;
+}
+
+EXPORT void baz()
+{
+  bar();
 }
